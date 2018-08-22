034256c03b6bbdbf1e77a6504af2bef785783913
Christian Clausen <csbc92@hotmail.com>
2018-08-22 10:14:58 +0200

    Added test

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     /**
-    * This is a fine test
+    * This is a fine foo test
     */
     @Test
     public void foo() {
@@ -15,6 +15,17 @@ public class GildedRoseTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+    }
+
+    /**
+    * This is a fine bar test
+    */
+    @Test
+    public void bar() {
+        Item[] items = new Item[] { new Item("bar", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("bar", app.items[0].name);
     }
 
 }
